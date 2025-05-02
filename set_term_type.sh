630e04403779dfe326d78d21cfd99ed16fed5043
mist941 <statkevich.ivan999@gmail.com>
2025-05-02 09:15:55 +0300

    added check for xterm-256color

--- set_term_type.sh
+++ set_term_type.sh
@@ -6,16 +6,20 @@ if [[ ! -v TERM ]] || ! command -v tput &>/dev/null; then
     echo "Skipping terminal setup: missing TERM or tput"
     exit 0
 fi
-    
-
-readonly COLORS=$(tput colors)
-
-if [[ "$COLORS" -ge 256 ]]; then
-    export TERM="xterm-256color"
-fi
-
 
 if [[ -v TMUX ]]; then
     export TERM="tmux-256color"
     exit 0
+fi
+
+if [[ -v STY ]]; then
+    export TERM="screen-256color"
+    exit 0
+fi
+
+if [[ ! -v TMUX && ! -v STY ]]; then
+    if [[ $(tput colors) -ge 256 ]]; then
+        export TERM="xterm-256color"
+        exit 0
+    fi
 fi
